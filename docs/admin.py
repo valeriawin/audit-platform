d99e02dd81f5bacc390a5ac1c7ae2c706cfae35e
Valerijachka <68111216+valeriawin@users.noreply.github.com>
2021-10-14 16:03:27 +0300

    refactor: add space

--- docs/admin.py
+++ docs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Stock, DocsRequest
 
+
 class CompanyAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['client_id']}),
