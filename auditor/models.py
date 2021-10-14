1f41e824658cb262dd42f124d342ab9ca75e33ee
Valerijachka <68111216+valeriawin@users.noreply.github.com>
2021-10-14 15:54:04 +0300

    refactor: add spaces

--- auditor/models.py
+++ auditor/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Auditor(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     profile_pic= models.ImageField(upload_to='profile_pic/Auditor/',null=True,blank=True)
@@ -10,12 +11,15 @@ class Auditor(models.Model):
     @property
     def get_name(self):
         return self.user.first_name+" "+self.user.last_name
+    
     @property
     def get_instance(self):
         return self
+    
     def __str__(self):
         return self.user.first_name+" "+self.user.last_name
 
+    
 class DocsAudit(models.Model): 
     auditor=models.ForeignKey(Auditor,on_delete=models.CASCADE)   
     info=models.CharField(max_length=100,default="Nothing")
@@ -24,5 +28,7 @@ class DocsAudit(models.Model):
     unit=models.PositiveIntegerField(default=0)
     status=models.CharField(max_length=20,default="Pending")
     date=models.DateField(auto_now=True)
+    
     def __str__(self):
         return self.auditor
+    
