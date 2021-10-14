d8f9c65b0efbeb2b7fd686c76d7b0e3057494eca
Valerijachka <68111216+valeriawin@users.noreply.github.com>
2021-10-14 15:52:25 +0300

    refactor: changed range

--- auditcompany/urls.py
+++ auditcompany/urls.py
@@ -16,11 +16,13 @@ urlpatterns = [
     path('admin-docs', views.admin_docs_view,name='admin-docs'),
     path('admin-auditor', views.admin_auditor_view,name='admin-auditor'),
     path('admin-client', views.admin_client_view,name='admin-client'),
-    path('update-auditor/<int:pk>', views.update_auditor_view,name='update-auditor'),
-    path('delete-auditor/<int:pk>', views.delete_auditor_view,name='delete-auditor'),
-    path('update-client/<int:pk>', views.update_client_view,name='update-client'),
-    path('delete-client/<int:pk>', views.delete_client_view,name='delete-client'),
     path('admin-audition', views.admin_audition_view,name='admin-audition'),
     path('approve-audition/<int:pk>/<int:unit>', views.approve_audition_view,name='approve-audition'),
     path('admin-request-history', views.admin_request_history_view,name='admin-request-history'),
+    
+    path('update-auditor/<int:pk>', views.update_auditor_view,name='update-auditor'),
+    path('delete-auditor/<int:pk>', views.delete_auditor_view,name='delete-auditor'),
+    
+    path('update-client/<int:pk>', views.update_client_view,name='update-client'),
+    path('delete-client/<int:pk>', views.delete_client_view,name='delete-client'),
 ]
